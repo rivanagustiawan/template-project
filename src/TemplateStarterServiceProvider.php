1a231102d40cc85b9be7b49ad45025c052587010
Rivan Agustiawan <nawaitsuga17@gmail.com>
2025-10-08 17:05:56 +0700

    fix

--- src/TemplateStarterServiceProvider.php
+++ src/TemplateStarterServiceProvider.php
@@ -8,12 +8,10 @@ class TemplateStarterServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Publikasi view (layout)
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/templatestarter'),
         ], 'views');
 
-        // Publikasi asset (CSS)
         $this->publishes([
             __DIR__ . '/../public' => public_path('vendor/templatestarter'),
         ], 'assets');
